86af03de4a809ae2330a6e70e76a8593ea2cc318
HW home <freizl@gmail.com>
2026-06-26 15:25:55 -0700

    sync

--- NaturalNumberGame/02_Addition.lean
+++ NaturalNumberGame/02_Addition.lean
@@ -1,5 +1,4 @@
 import NaturalNumberGame.MyNat
-import Mathlib
 
 namespace MyNat
 
@@ -21,9 +20,3 @@ example (a b c : ℕ) : a + (b + 0) + (c + 0) = a + b + c := by
 
 theorem succ_eq_add_one n : succ n = n + 1 := by
   rw [one_eq_succ_zero, add_succ, add_zero]
-
-example : (2 : ℕ) + 2 = 4 := by
-  -- sorry
-  nth_rewrite 2 [two_eq_succ_one]
-  rw [one_eq_succ_zero, add_succ, add_succ, add_zero]
-  rw [← three_eq_succ_two, ← four_eq_succ_three]
---
+++ NaturalNumberGame/07_AdvanceAddition.lean
@@ -0,0 +1,15 @@
+import NaturalNumberGame.MyNat
+
+namespace MyNat
+
+theorem add_right_cancel (a b n : ℕ) : a + n = b + n → a = b := by
+  induction n with
+   | zero =>
+     intro h1
+     rw [zero_eq_0, add_zero, add_zero] at h1
+     exact h1
+   | succ n hn =>
+     rw [add_succ, add_succ]
+     intro h2
+     apply succ_inj at h2
+     exact hn h2
