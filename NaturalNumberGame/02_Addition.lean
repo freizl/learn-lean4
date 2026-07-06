b77ce0ab1154c4805adfb93ff02a20d75ebe15a3
HW home <freizl@gmail.com>
2026-07-06 06:46:17 -0700

    game 2 and 3

--- NaturalNumberGame/02_Addition.lean
+++ NaturalNumberGame/02_Addition.lean
@@ -11,40 +11,49 @@ example (x y : ℕ) (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
 example : 2 = succ (succ 0) := by
   rw [← one_eq_succ_zero, ← two_eq_succ_one]
 
-
 example (a b c : ℕ) : a + (b + 0) + (c + 0) = a + b + c := by
   rw [add_zero, add_zero]
 
 example (a b c : ℕ) : a + (b + 0) + (c + 0) = a + b + c := by
   rw [add_zero c, add_zero b]
 
-theorem succ_eq_add_one n : succ n = n + 1 := by
-  rw [one_eq_succ_zero, add_succ, add_zero]
-
-theorem add_zero_right (a : ℕ) : a + 0 = a := by
-  sorry
-
 theorem zero_add (n : ℕ) : 0 + n = n := by
-  sorry
+  induction n with
+   | zero =>
+     rw [zero_eq_0]
+     rw [add_zero]
+   | succ d hd =>
+     rw [add_succ, hd]
+
+
 theorem succ_add (a b : ℕ) : succ a + b = succ (a + b) := by
-  sorry
+  induction b with
+  | zero =>
+     rw [zero_eq_0]
+     rw [add_zero, add_zero]
+  | succ d hd =>
+     rw [add_succ, add_succ, hd]
 
 theorem add_comm (a b : ℕ) : a + b = b + a := by
-  sorry
+  induction b with
+  | zero =>
+     repeat rw [zero_eq_0]
+     rw [add_zero, zero_add]
+  | succ d hd =>
+     rw [add_succ, succ_add, hd]
 
 theorem add_assoc (a b c : ℕ) : a + b + c = a + (b + c) := by
-  sorry
+  induction c with
+  | zero =>
+     repeat rw [zero_eq_0]
+     rw [add_zero, add_zero]
+  | succ d hd =>
+     rw [add_succ, add_succ, add_succ, hd]
 
 theorem add_right_comm (a b c : ℕ) : a + b + c = a + c + b := by
-  sorry
+  rw [add_assoc, add_comm b c, ← add_assoc]
 
 theorem add_left_comm (a b c : ℕ) : a + (b + c) = b + (a + c) := by
-  sorry
-
-theorem succ_add_right (a b : ℕ) : a + succ b = succ (a + b) := by
-  sorry
-
-theorem one_plus_one_eq_two : 1 + 1 = 2 := by
-  sorry
+  rw [← add_assoc, add_comm a b, add_assoc]
 
 end MyNat
--- NaturalNumberGame/03_Multiplication.lean
+++ NaturalNumberGame/03_Multiplication.lean
@@ -1,4 +1,5 @@
 import NaturalNumberGame.MyNat
+import «NaturalNumberGame».«01_Tutorial»
 import «NaturalNumberGame».«02_Addition»
 
 namespace MyNat
@@ -7,27 +8,53 @@ theorem mul_one (m : ℕ) : m * 1 = m := by
   rw [one_eq_succ_zero, mul_succ, mul_zero, zero_add]
 
 theorem zero_mul (m : ℕ) : 0 * m = 0 := by
-  sorry
+  induction m with
+  | zero => rw [zero_eq_0, mul_zero]
+  | succ d hd =>
+     rw [mul_succ, hd, add_zero]
 
 theorem succ_mul (a b : ℕ) : succ a * b = a * b + b := by
-  sorry
-
-theorem mul_zero (m : ℕ) : m * 0 = 0 := by
-  sorry
-
-theorem one_mul (m : ℕ) : 1 * m = m := by
-  sorry
+  induction b with
+  | zero => rw [zero_eq_0, mul_zero, mul_zero, add_zero]
+  | succ d hd =>
+      rw [mul_succ, mul_succ, hd, add_succ, add_succ]
+      rw [add_assoc, add_assoc, add_comm a d]
 
 theorem mul_comm (a b : ℕ) : a * b = b * a := by
-  sorry
+  induction b with
+  | zero => rw [zero_eq_0, mul_zero, zero_mul]
+  | succ d hd =>
+      rw [mul_succ, succ_mul, hd]
 
-theorem mul_assoc (a b c : ℕ) : (a * b) * c = a * (b * c) := by
-  sorry
+
+theorem one_mul (m : ℕ) : 1 * m = m := by
+  induction m with
+  | zero => rw [zero_eq_0, mul_zero]
+  | succ d hd => rw [mul_succ, hd, ← succ_eq_add_one]
+
+theorem one_mul_2 (m : ℕ) : 1 * m = m := by
+  rw [mul_comm, mul_one]
+
+theorem two_mul (m : ℕ): 2 * m = m + m := by
+  rw [mul_comm, two_eq_succ_one, mul_succ, mul_one]
 
 theorem mul_add (a b c : ℕ) : a * (b + c) = a * b + a * c := by
-  sorry
+  induction c with
+  | zero =>
+     rw [zero_eq_0, add_zero, mul_zero, add_zero]
+  | succ d hd =>
+     rw [add_succ, mul_succ, mul_succ, hd, add_assoc]
+
 
 theorem add_mul (a b c : ℕ) : (a + b) * c = a * c + b * c := by
-  sorry
+  rw [mul_comm, mul_add, mul_comm a c]
+
+
+theorem mul_assoc (a b c : ℕ) : (a * b) * c = a * (b * c) := by
+  induction c with
+  | zero => rw [zero_eq_0, mul_zero, mul_zero, mul_zero]
+  | succ d hd =>
+    rw [mul_succ, mul_succ, mul_add, hd]
+
 
 end MyNat
