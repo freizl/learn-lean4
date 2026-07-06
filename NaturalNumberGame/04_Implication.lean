16520ab6205702f1f64e71914c655021e4be7f55
HW home <freizl@gmail.com>
2026-07-06 07:32:13 -0700

    game 4 and 7

--- NaturalNumberGame/04_Implication.lean
+++ NaturalNumberGame/04_Implication.lean
@@ -1,38 +1,68 @@
 import NaturalNumberGame.MyNat
+import «NaturalNumberGame».«01_Tutorial»
+import «NaturalNumberGame».«02_Addition»
 
 namespace MyNat
 
-example (a b : ℕ) (h : a = b) : succ a = succ b := by
-  rw [h]
+example (x y z : ℕ) (h1 : x + y = 37) (h2 : 3 * x + z = 42) : x + y = 37 := by
+  exact h1
 
-theorem succ_eq_succ_of_eq (a b : ℕ) (h : a = b) : succ a = succ b := by
-  rw [h]
+example (x y : ℕ) (h : 0 + x = 0 + y + 2) : x = y + 2 := by
+  rw [zero_add, zero_add] at h
+  exact h
 
-theorem zero_ne_succ' (a : ℕ) : 0 ≠ succ a := by
-  sorry
+example (x y : ℕ) (h1 : x = 37) (h2 : x = 37 → y = 42) : y = 42 := by
+  apply h2 at h1
+  exact h1
 
-theorem eq_of_succ_eq_succ (a b : ℕ) (h : succ a = succ b) : a = b := by
-  sorry
+example (x : ℕ) (h : x + 1 = 4) : x = 3 := by
+  rw [one_eq_succ_zero, four_eq_succ_three, add_succ, add_zero] at h
+  apply succ_inj at h
+  exact h
 
-theorem succ_inj' (a b : ℕ) (h : succ a = succ b) : a = b := by
-  sorry
+-- Arguing backwards
+example (x : ℕ) (h : x + 1 = 4) : x = 3 := by
+  apply succ_inj
+  rw [← four_eq_succ_three]
+  rw [succ_eq_add_one]
+  exact h
 
-theorem not_succ_eq_zero (a : ℕ) : succ a ≠ 0 := by
-  sorry
 
-theorem zero_ne_succ'' (a : ℕ) : 0 ≠ succ a := by
-  sorry
+example (x : ℕ) : x = 37 → x = 37 := by
+  intro h
+  exact h
 
-theorem eq_trans' (a b c : ℕ) (h1 : a = b) (h2 : b = c) : a = c := by
-  sorry
 
-theorem eq_symm' (a b : ℕ) (h : a = b) : b = a := by
-  sorry
+example (x y : ℕ) : x + 1 = y + 1 → x = y := by
+  rw [← succ_eq_add_one, ← succ_eq_add_one]
+  apply succ_inj
 
-theorem false_elim' (a : Prop) : False → a := by
-  sorry
 
-theorem not_false : ¬ False := by
-  sorry
+example (x y : ℕ) (h1 : x = y) (h2 : x ≠ y) : False := by
+  apply h2 at h1
+  exact h1
+
+
+theorem zero_ne_one : (0: ℕ) ≠ 1 := by
+  intro h
+  rw [one_eq_succ_zero] at h
+  apply zero_ne_succ at h
+  exact h
+
+
+theorem one_ne_zero : (1 : ℕ) ≠ 0 := by
+  symm
+  exact zero_ne_one
+
+-- 2+2 != 5.
+example : succ (succ 0) + succ (succ 0) ≠ succ (succ (succ (succ (succ 0)))) := by
+  intro h
+  rw [add_succ, add_succ, add_zero] at h
+  apply succ_inj at h
+  apply succ_inj at h
+  apply succ_inj at h
+  apply succ_inj at h
+  apply zero_ne_succ at h
+  exact h
 
 end MyNat
--- NaturalNumberGame/07_AdvanceAddition.lean
+++ NaturalNumberGame/07_AdvanceAddition.lean
@@ -1,4 +1,6 @@
 import NaturalNumberGame.MyNat
+import «NaturalNumberGame».«01_Tutorial»
+import «NaturalNumberGame».«02_Addition»
 
 namespace MyNat
 
@@ -16,16 +18,38 @@ theorem add_right_cancel (a b n : ℕ) : a + n = b + n → a = b := by
      exact hn h2
 
 theorem add_left_cancel (a b n : ℕ) : n + a = n + b → a = b := by
-  sorry
+  rw [add_comm n a, add_comm n b]
+  exact add_right_cancel a b n
 
 theorem add_right_eq_self (a b : ℕ) : a + b = a → b = 0 := by
-  sorry
+  nth_rewrite 2 [← add_zero a] -- a + b = a + 0 -> b = 0
+  exact add_left_cancel b 0 a
 
 theorem add_left_eq_self (a b : ℕ) : b + a = a → b = 0 := by
-  sorry
+  nth_rewrite 2 [← zero_add a] -- b + a = 0 + a -> b = 0
+  exact add_right_cancel b 0 a
 
 theorem add_right_eq_zero (a b : ℕ) : a + b = 0 → a = 0 := by
-  sorry
+  cases b with
+   | zero =>
+      rw [zero_eq_0]
+      rw [add_zero]
+      -- TODO: any shortcut for "intro h; exact h;"
+      intro h
+      exact h
+   | succ d =>
+     rw [add_succ]
+     intro h2
+     symm at h2
+     apply zero_ne_succ at h2
+    --  Another example: if you have a hypothesis h : False then you are done,
+    -- because a false statement implies any statement.
+    -- Here cases h will close the goal, because there are no ways to
+    -- make a proof of False! So you will end up with no goals,
+    -- meaning you have proved everything.
+     cases h2
+
 
 theorem add_left_eq_zero (a b : ℕ) : b + a = 0 → a = 0 := by
-  sorry
+  rw [add_comm]
+  exact add_right_eq_zero a b
